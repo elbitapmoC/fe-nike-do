057ecc902df84ad843221345d98e77300404f034
Ahkal <bp@sojourn.llc>
2022-09-23 16:43:06 -0400

    filters added

--- domEvents.js
+++ domEvents.js
@@ -1,6 +1,8 @@
 const list = document.querySelector("#book-list ul");
 const userInput = document.querySelector("#add-book input");
 const form = document.querySelector("#add-book");
+const hideBooks = document.getElementById("hideBooks");
+const filter = document.querySelector("#search-books input");
 // Deletes books
 list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
@@ -52,4 +54,25 @@ form.addEventListener("submit", (e) => {
     deleteButton.removeAttribute("title");
     console.log("removeAttribute: title");
   }
+});
+
+hideBooks.addEventListener("change", () => {
+  // extra safety check
+  if (hideBooks.checked) {
+    list.classList.add("hideAll");
+  } else list.classList.remove("hideAll");
+});
+
+// filter books
+filter.addEventListener("keyup", (e) => {
+  const term = e.target.value.toLowerCase();
+  const books = document.querySelectorAll("ul li");
+  books.forEach((book) => {
+    const title = book.innerText.toLowerCase();
+    if (title.indexOf(term) != -1) {
+      book.style.display = "block";
+    } else {
+      book.style.display = "none";
+    }
+  });
 });
